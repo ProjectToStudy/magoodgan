c92f75a76c0c4ded7434dba230e8a59652898e69
jeongmiri <1889543@naver.com>
2022-11-17 18:54:19 +0900

    chore: allowed host 수정

--- backend/config/settings/settings.py
+++ backend/config/settings/settings.py
@@ -39,7 +39,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '0.0.0.0',
-    'magoodgan.p-e.kr',
+    'magoodgan.cf',
 ]
 
 
@@ -78,8 +78,8 @@ CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:8000',
     'http://0.0.0.0:8000',
     'http://0.0.0.0:3000',
-    'http://magoodgan.p-e.kr:8000',
-    'http://magoodgan.p-e.kr:3000',
+    'https://magoodgan.cf:8000',
+    'https://magoodgan.cf:3000',
     'http://localhost:3000',
     'http://localhost:8000'
 ]
