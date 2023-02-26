8782ba55b40a05be06be4489c1741aa1c248c09e
jeongmiri <1889543@naver.com>
2023-02-26 15:38:49 +0900

    fix: cors origin whitelist 수정

--- backend/config/settings/settings.py
+++ backend/config/settings/settings.py
@@ -78,8 +78,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:8000',
     'http://0.0.0.0:8000',
     'http://0.0.0.0:3000',
-    'https://magoodgan.cf:8000',
-    'https://magoodgan.cf:3000',
+    'https://magoodgan.cf',
     'http://localhost:3000',
     'http://localhost:8000'
 ]
